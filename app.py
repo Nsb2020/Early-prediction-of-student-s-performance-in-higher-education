2bff94ab9d2ed0ad78e1b8e1a7b827c50d007cf6
adeke910 <adekemibamiteko@gmail.com>
2023-06-20 10:11:44 +0100

    commit

--- app.py
+++ app.py
@@ -1,8 +1,52 @@
-from flask import Flask
+from flask import Flask, request, render_template
+# import sklearn
+# import pickle
+# import joblib
+# import pandas as pd
 
-app = Flask(__name__)
+
+# Use pickle to load in the trained model
+# with open(f'model/.pkl', 'rb') as f:
+# model = joblib.load('model/.pkl')
+
+
+app = Flask(__name__, template_folder='templates',
+            static_folder='static')
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('main.html')
+
+
+@app.route("/submit", methods=['POST'])
+def form_submit():
+    Age = request.json['age']
+    Course = request.json['course']
+    Tuition = request.json['tuition']
+    Pclass = request.json['pclass']
+    Curricular1A = request.json['curricular1A']
+    Curricular1E = request.json['curricular1E']
+    Curricular1G = request.json['curricular1G']
+    Curricular2A = request.json['curricular2A']
+    Curricular2E = request.json['curricular2E']
+    Curricular2G = request.json['curricular2G']
+
+    data = {
+        'Age': Age
+    }
+    print(data)
+
+    # df = pd.DataFrame.from_dict([data])
+    # print(df)
+    # return render_template('main.html')
+
+    # Get the model prediction
+    # prediction = model.predict(df)
+    # print(prediction)
+    # To render the form again, but add in the predictions and remind the user of previous input
+    return render_template('main.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
