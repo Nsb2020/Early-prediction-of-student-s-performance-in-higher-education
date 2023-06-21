641c117dc5239498a080c5f8fa6d7e9592ed828d
adeke910 <adekemibamiteko@gmail.com>
2023-06-21 11:13:36 +0100

    Final commit

--- model_deployment/app.py
+++ model_deployment/app.py
@@ -1,52 +1,65 @@
-from flask import Flask, request, render_template
-import sklearn
-import pickle
+from flask import Flask, request, render_template, jsonify
 import joblib
 import pandas as pd
 
 
-# Use pickle to load in the trained model
-# with open(f'model/titanic_model_stkd.pkl', 'rb') as f:
-model = joblib.load('model/ensemble_classifier_model.pkl')
+# Load the trained model
+model = joblib.load('model/rf_classifier_model.pkl')
 
-app = Flask(__name__, template_folder='templates',
-            static_folder='static')
-
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('main.html')
 
-
-@app.route("/submit", methods=['POST'])
+@app.route("/submit", methods=['POST', 'GET'])
 def form_submit():
+    name = request.json['name']
     Age = request.json['age']
     Course = request.json['course']
     Tuition = request.json['tuition']
-    Pclass = request.json['pclass']
     Curricular1A = request.json['curricular1A']
-    Curricular1E = request.json['curricular1E']
-    Curricular1G = request.json['curricular1G']
+    Curricular1EV = request.json['curricular1EV']
+    Curricular1ER = request.json['curricular1ER']
     Curricular2A = request.json['curricular2A']
-    Curricular2E = request.json['curricular2E']
+    Curricular2ER = request.json['curricular2ER']
+    Curricular2EV = request.json['curricular2EV']
     Curricular2G = request.json['curricular2G']
 
     data = {
-        'Age': Age,
-        #'Curricular units for 1st semester': 
+        0: Curricular1A,
+        1: Curricular2A,
+        2: Tuition,
+        3: Course,
+        4: Curricular2ER,
+        5: Age,
+        6: Curricular1EV,
+        7: Curricular2EV,
+        8: Curricular2G,
+        9: Curricular1ER
     }
-    print(data)
 
-    # df = pd.DataFrame.from_dict([data])
-    # print(df)
-    # return render_template('main.html')
+    df = pd.DataFrame.from_dict([data])
 
-    # Get the model prediction
-    # prediction = model.predict(df)
-    # print(prediction)
-    # To render the form again, but add in the predictions and remind the user of previous input
-    return render_template('main.html')
+    prediction = model.predict(df)
 
+    if int(prediction) == 0:
+        result = 'Dropout'
+    elif int(prediction) == 1:
+        result = 'Enrolled'
+    else:
+        result = 'Graduate'
+    return jsonify(result=result, name=name)
+
+    name = data.get('name', '')
+    return redirect(url_for('show_result', result=result, name=name))
+
+@app.route("/result")
+def show_result():
+    result = request.args.get("result", "")
+    name = request.args.get("name", "")
+    return render_template("result.html", result=result)
+    return render_template("result.html", result=result, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
